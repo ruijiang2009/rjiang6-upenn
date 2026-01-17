4bafba7df378affb0de61f3cf27b0bea8611caef
Rui Jiang <rui.jiang.dev@gmail.com>
2026-01-17 15:32:57 -0800

    Update HW1

--- ese5460/Assignments/HW1/MySubmission/62502470_hw1_problem1.py
+++ ese5460/Assignments/HW1/MySubmission/62502470_hw1_problem1.py
@@ -36,9 +36,10 @@ def get_data():
     #         to be written within the single quotes
     ds = fetch_openml('mnist_784')
     ###TODO: Get the MNIST image data and target, convert to numpy and cast the data into either float or integer! 
-    x, y = 
+    x, y = ds.data.to_numpy().astype(float), ds.target.to_numpy().astype(int)
     ### TODO: Plot one of the images with plt.imshow
-    ...
+    plt.imshow(x[0].reshape(28, 28), cmap='gray')
+    plt.show()
     return x, y
 
 
@@ -57,7 +58,17 @@ def resize(x, size=(14, 14)):
     Return: the numpy array of resized, downsampled MNIST data of shape (num_of_images, 196)
     """
     ###TODO###
-    ...
+    d_sampled = []
+    for i in range(x.shape[0]):
+        img = x[i].reshape(28, 28)
+        resized_img = cv2.resize(img, size)
+        d_sampled.append(resized_img.flatten())
+    d_sampled = np.array(d_sampled)
+
+    # Plot one resized image to check
+    plt.imshow(d_sampled[0].reshape(size), cmap='gray')
+    plt.show()
+
     return d_sampled
 
 
@@ -76,7 +87,21 @@ def subsample(x, y, num=10000):
 
     """
     ### TODO
-    ...
+    # samples_per_class = num // 10  # 1000 samples per digit
+    samples_per_class = 1000
+    x_small = []
+    y_small = []
+
+    for digit in range(10):
+        indices = np.where(y == digit)[0] # Get all indices for this digit
+        # Randomly select samples_per_class indices
+        selected_indices = np.random.choice(indices, samples_per_class, replace=False)
+        x_small.append(x[selected_indices])
+        y_small.append(y[selected_indices])
+
+    x_small = np.vstack(x_small)
+    y_small = np.hstack(y_small)
+
     return x_small, y_small
 
 
@@ -95,9 +120,9 @@ def data_preprocessing(x,y):
     Return: x_train, x_val, y_train, y_val
     """
     ###TODO###
-    x, y = subsample(...)
-    x = resize(...)
-    x_train, x_val, y_train, y_val = train_test_split(..., ..., test_size=..., random_state=..., shuffle=..., stratify=...)
+    x, y = subsample(x, y)
+    x = resize(x)
+    x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2, random_state=42, shuffle=True, stratify=y)
     return x_train, x_val, y_train, y_val
 
 
@@ -115,7 +140,7 @@ def get_number_of_support_samples(clf):
     Return: Total number of support samples given the classifier
     """
     ###TODO###
-    ...
+    return len(clf.support_vectors_) 
 
 
 def train_test_SVM(x_train, y_train, x_val, y_val):
@@ -132,12 +157,21 @@ def train_test_SVM(x_train, y_train, x_val, y_val):
     """
     ###TODO###
     # initialize the classifier and get the classifier score
-    clf = ...
-    clf.fit(...)
-    y_pred = clf.predict(...)
-    val_acc = clf.score(...)
-    cm = ...
-    supp_vector_percent = ... 
+    clf = svm.SVC()
+    clf.fit(x_train, y_train)
+    y_pred = clf.predict(x_val)
+    val_acc = clf.score(x_val, y_val)
+    cm = confusion_matrix(y_val, y_pred)
+
+    # Calculate support vector ratio
+    num_support_vectors = get_number_of_support_samples(clf)
+    supp_vector_percent = (num_support_vectors / len(x_train)) * 100
+
+    # Print results
+    print(f"Validation Accuracy: {val_acc:.4f}")
+    print(f"Validation Error: {1 - val_acc:.4f}")
+    print(f"Confusion Matrix:\n{cm}")
+    print(f"Support Vector Ratio: {supp_vector_percent:.2f}%")
 
     return val_acc, cm, supp_vector_percent
 
@@ -162,9 +196,20 @@ def grid_search_SVM(x_train, y_train, x_val, y_val, params):
     Return: best classifier found by grid search
     """
     ###TODO###
-    clf = GridSearchCV(...)
-    clf.fit(...)
-    ...
+    clf = GridSearchCV(svm.SVC(), params, cv=5, verbose=1)
+    clf.fit(x_train, y_train)
+    
+    # Get best values
+    print(f"Best accuracy for training data: {clf.best_score_:.4f}")
+    print(f"Best estimators: {clf.best_params_}")
+    print(f"All cross-validation results: {clf.cv_results_['mean_test_score']}")
+    
+    # Get predicted y_val using best classifier
+    y_pred = clf.best_estimator_.predict(x_val)
+    
+    # Print confusion matrix
+    cm = confusion_matrix(y_val, y_pred)
+    print(f"Confusion Matrix:\n{cm}")
 
     return clf
 
@@ -188,10 +233,24 @@ def apply_gfilter(images, frequency_list, theta_list, bandwidth_list):
         gabor_real_coeffs: np.array of size (num of images, num of filters * 14 * 14)
     """
 
-    gabor_real_coeffs = ...
-    for img in tqdm(range(images.shape[0])):
+    gabor_real_coeffs = []
+    for img_idx in tqdm(range(images.shape[0])):
         ### TODO ###
-        ....
+        img = images[img_idx].reshape(14, 14)
+        img_coeffs = []
+        
+        # Apply each filter combination
+        for freq in frequency_list:
+            for theta in theta_list:
+                for bandwidth in bandwidth_list:
+                    # Apply gabor filter and get real coefficients
+                    real, _ = gabor(img, frequency=freq, theta=theta, bandwidth=bandwidth)
+                    img_coeffs.append(real.flatten())
+        
+        # Concatenate all filter responses for this image
+        gabor_real_coeffs.append(np.concatenate(img_coeffs))
+    
+    gabor_real_coeffs = np.array(gabor_real_coeffs)
 
     n_filters = len(frequency_list) * len(theta_list) * len(bandwidth_list)
     assert gabor_real_coeffs.shape[0] == len(images)
@@ -207,26 +266,78 @@ def gabor_filter(x_train, y_train, x_val, y_val):
     # 1. Prepare a balanced 1,000 training and validation images
     #     of 100 images per class
     ### TODO ###
+    x_train_small, y_train_small = subsample(x_train, y_train, num=1000)
+    x_val_small, y_val_small = subsample(x_val, y_val, num=1000)
 
     # 2. Check gabor kernel and gabor outputs as example
     ### TODO ###
-
+    kernel = gabor_kernel(frequency=0.1, theta=0, bandwidth=1.0)
+    sample_img = x_train_small[0].reshape(14, 14)
+    real, imag = gabor(sample_img, frequency=0.1, theta=0, bandwidth=1.0)
+    
+    # Plot example
+    plt.figure(figsize=(12, 4))
+    plt.subplot(1, 3, 1)
+    plt.imshow(sample_img, cmap='gray')
+    plt.title('Original Image')
+    plt.subplot(1, 3, 2)
+    plt.imshow(real, cmap='gray')
+    plt.title('Gabor Real')
+    plt.subplot(1, 3, 3)
+    plt.imshow(imag, cmap='gray')
+    plt.title('Gabor Imaginary')
+    plt.show()
 
     # 3. Diversify the values for frequency, theta, and bandwidth parameters
     #    Plot 8 pairs of real and imaginary coefficients from gabor_kernel()
     #    to check diversity of this filter-bank
     ### TODO ###
+    frequency_list = [0.1, 0.2]
+    theta_list = [0, np.pi/4, np.pi/2, 3*np.pi/4]
+    bandwidth_list = [1.0, 2.0]
+    
+    plt.figure(figsize=(16, 8))
+    for i, (freq, theta) in enumerate([(f, t) for f in frequency_list for t in theta_list[:4]]):
+        kernel = gabor_kernel(frequency=freq, theta=theta, bandwidth=1.0)
+        plt.subplot(2, 8, 2*i + 1)
+        plt.imshow(np.real(kernel), cmap='gray')
+        plt.title(f'f={freq}, θ={theta:.2f}\nReal')
+        plt.axis('off')
+        plt.subplot(2, 8, 2*i + 2)
+        plt.imshow(np.imag(kernel), cmap='gray')
+        plt.title(f'Imaginary')
+        plt.axis('off')
+    plt.tight_layout()
+    plt.show()
 
     # 4. Obtain new training and validation datasets of real coefficients
     #    by calling apply_gfilter() function
     ### TODO ###
+    print("Applying Gabor filters to training data...")
+    x_train_gabor = apply_gfilter(x_train_small, frequency_list, theta_list, bandwidth_list)
+    print("Applying Gabor filters to validation data...")
+    x_val_gabor = apply_gfilter(x_val_small, frequency_list, theta_list, bandwidth_list)
 
     # 5. Standardize and then PCA features with StandardScaler() and PCA() functions
     ### TODO ###
+    scaler = StandardScaler()
+    x_train_scaled = scaler.fit_transform(x_train_gabor)
+    x_val_scaled = scaler.transform(x_val_gabor)
+    
+    pca = PCA(n_components=50)
+    x_train_pca = pca.fit_transform(x_train_scaled)
+    x_val_pca = pca.transform(x_val_scaled)
+    
+    print(f"Explained variance ratio: {sum(pca.explained_variance_ratio_):.4f}")
 
     # 6. Find best svm.SVC classifier
     #    Hint: look into cache_size parameter of svm.SVC() to speed up training
     ### TODO ###
+    classifier = svm.SVC(kernel='rbf', C=1.0, gamma='scale', cache_size=1000)
+    classifier.fit(x_train_pca, y_train_small)
+    
+    val_acc = classifier.score(x_val_pca, y_val_small)
+    print(f"Gabor Filter SVM Validation Accuracy: {val_acc:.4f}")
 
     return classifier
 
@@ -234,23 +345,23 @@ def gabor_filter(x_train, y_train, x_val, y_val):
 
 if __name__ == '__main__':
     ### TODO: get downloaded MNIST data and target
-    x, y = ...
+    x, y = get_data()
 
     ### TODO: preprocess the data
-    x_train, x_val, y_train, y_val = ...
+    x_train, x_val, y_train, y_val = data_preprocessing(x, y)
 
     train_test_SVM(x_train, y_train, x_val, y_val)
 
     ### TODO: define SVM hyperparameters to perform grid search with
     params = {
-        'C':[...],
-              ...,
-              ...
+        'C': [0.1, 1, 10],
+        'kernel': ['linear', 'rbf'],
+        'gamma': ['scale', 'auto']
     }
     ### TODO: Save the best svm classifier returned by grid_search_SVM to pickle file
-    svm = grid_search_SVM(...)
+    best_svm = grid_search_SVM(x_train, y_train, x_val, y_val, params)
     with open('grid_search_svm.pkl', 'wb') as f:
-        pickle.dump(svm, f)
+        pickle.dump(best_svm, f)
 
     ### TODO: gabor_filter() and apply_gfilter()
-    ...
+    gabor_classifier = gabor_filter(x_train, y_train, x_val, y_val)
